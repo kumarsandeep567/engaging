dd19151e1d9379aa4e1a5526cf901ae6b27f6bba
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-07-18 00:23:45 -0400

    User's profile photo will now be shown, if available

--- resources/js/Components/App/UserAvatar.jsx
+++ resources/js/Components/App/UserAvatar.jsx
@@ -2,8 +2,47 @@
  * A component to display user avatar
  */
 
-const UserAvatar = () => {
+const UserAvatar = ({ user, online = null, profile = false }) => {
 
+    // Online status indicator with Daisy UI
+    // This will be used for CSS class
+    let onlineClass = online === true
+        ? "online"
+        : (
+            online === false
+            ? "offline"
+            : ""
+        );
+    
+    // Define how large should the profile photo be
+    const sizeClass = profile ? "w-40" : "w-8";
+
+    return (
+        <>
+            {/* If the user avatar exists then display the avatar*/}
+            {user.avatar_url && (
+                <div className = {`chat-image avatar ${onlineClass}`}>
+                    <div className = {`rounded-full ${sizeClass}`}>
+                        <img src = {user.avatar_url} alt = "profile photo" />
+                    </div>
+                </div>
+            )}
+
+            {/* 
+                If the user avatar does not exist then use a 
+                placeholder with the user's initials
+            */}
+            {!user.avatar_url && (
+                <div className = {`chat-image avatar placeholder ${onlineClass}`}>
+                    <div className = {`bg-gray-300 text-gray-800 rounded-full ${sizeClass} text-center`}>
+                        <span className = "text-xl">
+                            {user.name.substring(0, 1)}
+                        </span>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default UserAvatar;
