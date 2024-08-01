cef9a9a04f5053f5c2583b0969916382b4aa1ae8
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-07-31 22:58:55 -0400

    Added broadcast channels for personal chats and group chats

--- routes/channels.php
+++ routes/channels.php
@@ -1,17 +1,48 @@
 <?php
 
+use App\Models\User;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Broadcast;
 
 /**
  * Laravel Reverb (using Websockets)
+ */
+
+
+/**
  * Create a presence channel that allows authenticated 
  * users to join and know about other users in the 
  * channel. 
  */
-
-Broadcast::channel('online', function ($user) {
+Broadcast::channel('online', function (User $user) {
 
     // Return the UserResource if the user exists
     return $user ? new UserResource($user) : null;
+});
+
+
+/**
+ * Create a channel for every personal 1:1 chats.
+ */
+Broadcast::channel('message.user.{userId1}-{userId2}', function (
+    User $user, int $userId1, int $userId2
+) {
+    
+    // Every personal 1:1 chat will have their own communication channel
+    return ($user->id === $userId1 || $user->id === $userId2)
+    ? $user
+    : null;
+});
+
+
+/**
+ * Create a channel for every group chat.
+ * Ensure the user is a member of the group.
+ */
+Broadcast::channel('message.group.{groupId}', function (
+    User $user, int $groupId
+) {
+    return ($user->groups->contains('id', $groupId))
+    ? $user
+    : null;
 });
