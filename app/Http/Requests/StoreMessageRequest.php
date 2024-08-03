18f564ba31b5878ceee4fd301bce0ba8acd548df
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-08-03 01:09:07 -0400

    Added rules to authorize message storage

--- app/Http/Requests/StoreMessageRequest.php
+++ app/Http/Requests/StoreMessageRequest.php
@@ -8,10 +8,12 @@ class StoreMessageRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     * To prevent storing the messages in the database, simply set authorize()
+     * to return false.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,19 @@ class StoreMessageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'message'       => 'nullable|string',
+
+            // Group ID is needed, if Receiver ID is null
+            'group_id'      => 'required_without:receiver_id|nullable|exists:groups,id',
+
+            // Receiver ID is needed, if Group ID is null
+            'receiver_id'   => 'required_without:group_id|nullable|exists:users,id',
+
+            // Number of files that can be uploaded at a time
+            'attachments'   => 'nullable|array|max:10',
+
+            // Largest file size allowed is 1GB
+            'attachments.*' => 'file|max:1024000'
         ];
     }
 }
