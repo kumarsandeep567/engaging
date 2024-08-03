1c0abf64d424c2e51107c6aa8373c766d951bf5f
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-08-03 01:15:53 -0400

    A loading icon will be visible when sending a message

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -7,6 +7,7 @@ import {
     PaperAirplaneIcon
 } from "@heroicons/react/24/solid";
 import NewMessageInput from "@/Components/App/NewMessageInput";
+import axios from "axios";
 
 /**
  * This component will provide a bottom panel for the text field, attachment, 
@@ -18,6 +19,56 @@ const MessageInput = ({ conversation = null }) => {
     const [newMessage, setNewMessage] = useState("");
     const [inputErrorMessage, setInputErrorMessage] = useState("");
     const [messageSending, setMessageSending] = useState(false);
+
+    // Handler to send the message.
+    const sendMessage = () => {
+        if (newMessage.trim() === "") {
+            setInputErrorMessage("Cannot send an empty message. Please type something or select a file.");
+
+            // Hide the error message after 5 seconds.
+            setTimeout(() => {
+                setInputErrorMessage("");
+            }, 5000);
+
+            return;
+        }
+
+        // Use a form data to send the message along with attachments (if any).
+        const messageData = new FormData();
+        messageData.append("message", newMessage)
+
+        if (conversation.is_user) {
+            messageData.append("receiver_id", conversation.id);
+        } else if (conversation.is_group) {
+            messageData.append("group_id", conversation.id);
+        }
+
+        // Enable the loading spinner to indicate the message is being sent.
+        setMessageSending(true);
+
+        // Make a POST request to store the message to the database.
+        // If the message has any attachments, show a progress bar for the same.
+        axios.post(
+            route("message.store"),
+            messageData,
+            {onUploadProgress: (progressEvent) => {
+                const progress = Math.round(
+                    (progressEvent.loaded / progressEvent.total) * 100
+                );
+
+                // For now, just log the progress
+                console.log("Upload progress: ", progress);
+            }
+        }).then((response) => {
+            setNewMessage("");
+            setMessageSending(false);
+            console.log("UPLOAD RESPONSE: ", response);
+        }).catch((error) => {
+            setMessageSending(false);
+            console.log("UPLOAD ERROR: ", error);
+        });
+
+    };
 
 
     return (
@@ -83,13 +134,17 @@ const MessageInput = ({ conversation = null }) => {
                     <NewMessageInput 
                         value={newMessage}
                         onChange={(ev) => setNewMessage(ev.target.value)}
+                        onSend={sendMessage}
                     />
 
                     {/* The send message button */}
                     {/* When sending a message, if the message delivery takes
                         too long, show the loader animation.
                     */}
-                    <button className="btn bg-sky-400 rounded-xl text-gray-800 hover:bg-sky-300">
+                    <button 
+                        className="btn bg-sky-400 rounded-xl text-gray-800 hover:bg-sky-300"
+                        onClick={sendMessage}
+                    >
                         { messageSending && (
                             <span className="loading loading-spinner loding-xs"></span>
                         )}
