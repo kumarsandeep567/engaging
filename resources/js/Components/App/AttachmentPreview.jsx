5c4f1ff5ec29ef1bb815b3c2fc664c06cd4fbf8f
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-08-10 01:38:04 -0400

    Added icons for Video files as well

--- resources/js/Components/App/AttachmentPreview.jsx
+++ resources/js/Components/App/AttachmentPreview.jsx
@@ -1,20 +1,36 @@
 import React from "react";
-import { PaperClipIcon } from "@heroicons/react/24/outline";
-import { fileSizeInBytes, isPDF, isPreviewable } from "@/helpers";
+import {
+    DocumentTextIcon, 
+    FilmIcon, 
+    PaperClipIcon
+} from "@heroicons/react/24/outline";
+import {
+    fileSizeInBytes, 
+    isPDF, 
+    isPreviewable, 
+    isVideo, 
+    truncateText
+} from "@/helpers";
 
 const AttachmentPreview = ({ file }) => {
     return (
         <div className="w-full flex items-center gap-2 py-2 px-3 rounded-md bg-slate-300">
             <div>
-                {isPDF(file.file) && <img src="/img/pdf.png" alt="PDF" className="w-8" />}
+                
+                {/* Show icons based on file type (except audio files) */}
+                {isPDF(file.file) && <DocumentTextIcon className="w-8" />}
+                {isVideo(file.file) && <FilmIcon className="w-8" />}
                 {!isPreviewable(file.file) && (
-                    <div className="flex justify-center items-center w-10 h-10 bg-gray-200 rounded">
+                    <div className="flex justify-center items-center w-10 h-10 bg-gray-100 rounded">
                         <PaperClipIcon className="w-6" />
                     </div>
                 )}
             </div>
+
             <div className="flex-1 text-gray-400 text-nowrap text-ellipsis overflow-hidden">
-                <h3 className="text-slate-800">{file.file.name}</h3>
+
+                {/* Show the file name and file size */}
+                <h3 className="text-slate-800">{truncateText(file.file.name, 25)}</h3>
                 <p className="text-xs text-slate-500">
                     {fileSizeInBytes(file.file.size)}
                 </p>
