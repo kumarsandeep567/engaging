a7d0ce16da09bf02956bea875c927cb3dff11958
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-08-20 00:04:56 -0400

    Home layout will actively listen for `GroupDeleted` socket events and update the frontend accordingly

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -80,6 +80,19 @@ export default function Authenticated({ header, children }) {
                     }`
                 });
             });
+
+            if (conversation.is_group) {
+                Echo.private(`group.deleted.${conversation.id}`)
+                .listen("GroupDeleted", (e) => {
+                    emit("group.deleted", {
+                        id: e.id,
+                        name: e.name
+                    });
+                })
+                .error((e) => {
+                    console.error(e);
+                });
+            }
         });
 
         // Stop listening for any event in the broadcast channels
@@ -96,6 +109,10 @@ export default function Authenticated({ header, children }) {
                     .join("-")}`;
                 }
                 Echo.leave(channel);
+
+                if (conversation.is_group) {
+                    Echo.leave(`group.deleted.${conversation.id}`);
+                }
             });
         };
     }, [conversations]);
