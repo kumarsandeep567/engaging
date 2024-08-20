90e566d44e20299712fe045d687c5d419fbb5c59
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-08-20 00:02:03 -0400

    Icons will have a tooltip for better accessibility

--- resources/js/Components/App/GroupDescriptionPopover.jsx
+++ resources/js/Components/App/GroupDescriptionPopover.jsx
@@ -1,19 +1,22 @@
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
-import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
 
 export default function GroupDescriptionPopover({ description }) {
     return (
         <Popover className="relative">
             {({ open }) => (
                 <>
-                    <Popover.Button className={`${
-                        open 
-                        ? "text-gray-200"
-                        : "text-gray-400"
-                    } hover:text-gray-200`
-                    }>
-                        <ExclamationCircleIcon className="w-4" />
+                    <Popover.Button 
+                        className={`${
+                            open 
+                            ? "text-emerald-600"
+                            : "text-gray-800"} 
+                            hover:text-gray-500 tooltip tooltip-left`
+                        }
+                        data-tip="View description"
+                    >
+                        <InformationCircleIcon className="w-6" />
                     </Popover.Button>
                     <Transition
                         as={Fragment}
@@ -24,14 +27,14 @@ export default function GroupDescriptionPopover({ description }) {
                         leaveFrom="opacity-100 translate-y-0"
                         leaveTo="opacity-0 translate-y-1"
                     >
-                        <Popover.Panel className="absolute right-0 z-10 mt-3 w-[300px] px-4 sm:px-0">
+                        <Popover.Panel className="absolute right-0 z-50 mt-3 w-[300px] px-4 sm:px-0">
                             <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5">
-                                <div className="bg-gray-800 p-4">
-                                    <h2 className="text-lg mb-3">
-                                        Description
-                                    </h2>
+                                <div className="bg-gray-100 p-4">
+                                    <h4 className="text-md mb-3 select-none font-bold">
+                                        Group Description
+                                    </h4>
                                     {description && (
-                                        <div className="text-xs">
+                                        <div className="text-md">
                                             {description}
                                         </div>
                                     )}
