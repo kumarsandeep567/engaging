3d1ba54baaefa0ab9946d35413bdf690d4680f9c
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-08-21 20:30:26 -0400

    Added middleware for administrators

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AdminUser;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,7 +18,13 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        // Administrator middleware
+        $middleware->alias([
+            'admin' => AdminUser::class
+
+            // Enable the ActiveUser middleware if needed
+            // 'active' => ActiveUser::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
