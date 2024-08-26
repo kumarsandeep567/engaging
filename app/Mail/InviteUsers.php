a519779d41b8fc2cf127bf86e5d7c6a8af931f04
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-08-26 15:54:04 -0400

    Added email notification for inviting new users

---
+++ app/Mail/InviteUsers.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class InviteUsers extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public User $user, public string $password)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Invitation to join '. env('APP_NAME') .' Messenger!',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        // Use a Markdown view instead of a Blade view
+        return new Content(
+            markdown: 'mail.user.invited',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
